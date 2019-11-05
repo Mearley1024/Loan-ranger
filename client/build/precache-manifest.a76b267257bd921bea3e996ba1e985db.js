e1dd9df9dcf80025109b681cb398a6b4118f215a
Marcela Earley <Marcela.earley1024@gmail.com>
2019-11-04 21:52:12 -0800

    Initial Commit

---
+++ client/build/precache-manifest.a76b267257bd921bea3e996ba1e985db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d1e813f52e4efa93611ad9ea81ee35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1371db0a443162d98a94",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4c0f9071d8f2c0f60df6",
+    "url": "/static/js/2.874cb207.chunk.js"
+  },
+  {
+    "revision": "1371db0a443162d98a94",
+    "url": "/static/js/main.49267668.chunk.js"
+  },
+  {
+    "revision": "fe54835287933d8a193f",
+    "url": "/static/js/runtime-main.86296b92.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
