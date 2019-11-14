f5e85f61695d092df47bbcb880b3bb241dbdd006
Daniel Gomez <ss.im.back@gmail.com>
2019-11-13 21:24:53 -0800

    term page and app

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Alert } from 'reactstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './pages/Home/index.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Terms } from './pages/Terms/Terms.js';
+import { Link } from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Alert color="primary">
-        LOAN RANGER — check it out!
-      </Alert>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Terms" component={Terms} />
+       </Switch>
+    </Router>
+
   );
 }
 
