3cd8e69caa8a502e973e854883684752b8d4f008
Dan <1danabana@gmail.com>
2019-11-16 13:52:12 -0800

    renaming files and adding navbars to components with menues

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
+import Home from './pages/Home/index';
 import './App.css';
 import { Alert } from 'reactstrap';
+import NoMatch from "./pages/Nothing-is-here";
+import Nav from './components/Header/nav';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Alert color="primary">
+//         LOAN RANGER — check it out!
+//       </Alert>
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 function App() {
   return (
-    <div className="App">
-      <Alert color="primary">
-        LOAN RANGER — check it out!
-      </Alert>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
