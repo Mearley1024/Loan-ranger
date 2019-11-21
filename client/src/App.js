98ad5d747a24d887593f0e165a1c1d3ca90a9f72
Daniel Gomez <ss.im.back@gmail.com>
2019-11-20 19:37:42 -0800

    landing page fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import  Homes  from './pages/Home/Home.component.js';
+import  Home  from './pages/Home/Home.component.js';
 import  Terms  from './pages/Terms/Terms.component.js';
 import  Qualifications  from './pages/Qualifications/Qualifications.component.js';
 // import { Link } from 'react-router-dom'
 import  LoanType  from './pages/LoanType/LoanType.component.js';
-import Form from 'react-bootstrap/Form';
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
-import Nav from 'react-bootstrap/Nav'
 import Header from './components/Header/navbar.component';
 
 
@@ -25,7 +19,8 @@ function App() {
     <Router>
     <Header/> 
       <Switch>
-        <Route exact path="/Home" component={Homes} />
+      <Route exact path="/" component={Home} />
+        <Route exact path="/Home" component={Home} />
         <Route exact path="/Terms" component={Terms} />
         <Route exact path="/LoanType" component={LoanType} />
         <Route exact path="/Qualifications" component={Qualifications} />
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+export default class Homes extends React.Component {
+     render(){
+        return(
+          <Form className="login">
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Keep me signed in" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+        );
+    }
+}
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
