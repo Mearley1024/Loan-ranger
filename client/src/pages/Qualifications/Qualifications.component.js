1b2cc1c1a38524fa8908601090526bfb40b3b52b
Dtferrer <51558887+Dtferrer@users.noreply.github.com>
2019-11-23 10:41:16 -0800

    Update Qualifications.component.js

--- client/src/pages/Qualifications/Qualifications.component.js
+++ client/src/pages/Qualifications/Qualifications.component.js
@@ -48,7 +48,7 @@ export default class Qualifications extends React.Component {
 </Form>
 <br/>
                   </div>
-                </dic>
+                </div>
               </div>
               
           </div>
