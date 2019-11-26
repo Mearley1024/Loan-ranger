f77f9b1c949656537218a317ec8dabe66484b79e
Danica Ferrer <dtferrer@ucsd.edu>
2019-11-25 20:45:59 -0800

    Made the Empty result better looking

--- client/src/components/Empty/index.js
+++ client/src/components/Empty/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function EmptyList(){
     return(
-        <div>
-            <h2>No Loans To Display</h2>
-        </div>
+        <Card.Header as="h5"> <Img className='imgs' src={logo1} crossorigin="anonymous" />Loan List</Card.Header>
+         <Card.Body>
+             <Card.Title>No Loans to Display</Card.Title>
+             <Card.Text>
+                 Unfortunately, no current loans match your filters. Please, try again.
+             </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+     </Card>
     )
 }
