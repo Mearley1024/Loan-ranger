f5eeb977f03f5d37c0eaecbd8f94b89a4559b73b
Dan <1danabana@gmail.com>
2019-12-02 20:19:55 -0800

    debugged app.js in src file so that the app loaded up

--- client/src/App.js
+++ client/src/App.js
@@ -9,35 +9,13 @@ import  Terms  from './pages/Terms/Terms.component.js';
 import  Qualifications  from './pages/Qualifications/Qualifications.component.js';
 // import { Link } from 'react-router-dom'
 import  LoanType  from './pages/LoanType/LoanType.component.js';
-<<<<<<< HEAD
-import Header from './components/Header/navbar.component.js';
-import NavBar from "./components/Header/LoginNav";
-import { useAuth0 } from "./react-auth0-spa";
-
-
-
-function App() {
-  console.log('====loading=========')
-  console.log(useAuth0)
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-=======
 import Header from './components/Header/navbar.component';
 import Results from './pages/Results/Results.component';
->>>>>>> 59e525deaf102e8b6ed945d51b6e4396bf077dcf
 
+function App() {
   return (
-<<<<<<< HEAD
-    <Router >
-      <NavBar />
-    <Header/> 
-=======
     <Router>
     <Header /> 
->>>>>>> 59e525deaf102e8b6ed945d51b6e4396bf077dcf
       <Switch>
       <Route exact path="/" component={Home} />
         <Route exact path="/Home" component={Home} />
@@ -49,6 +27,6 @@ import Results from './pages/Results/Results.component';
     </Router>
 
   );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -10,36 +10,8 @@ import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
 import history from "./utils/history";
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// A function that routes the user to the right place
-// after login
-const onRedirectCallback = appState => {
-  history.push(
-    appState && appState.targetUrl
-      ? appState.targetUrl
-      : window.location.pathname
-  );
-};
-
-ReactDOM.render(
-  <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
-    redirect_uri={window.location.origin}
-    onRedirectCallback={onRedirectCallback}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
-=======
 
 ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> 59e525deaf102e8b6ed945d51b6e4396bf077dcf
 
 
 export default class Homes extends React.Component {
